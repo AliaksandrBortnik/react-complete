db8c28b98513184a5c12de0605ec64fde8a45898
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-14 22:40:53 +0300

    Style expense item component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <p>Expense output</p>
+    <div className="expense-item">
+      <div>February 2022</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$100.55</div>
+      </div>
+    </div>
   );
 }
 
