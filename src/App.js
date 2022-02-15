5a4877a8c5f3771f2e1749a8f265741d2d44be18
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-15 10:32:32 +0300

    Pass data to component as props object

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurance', amount: 100.55, date: new Date()},
+    { title: 'Car Insurance2', amount: 200.55, date: new Date()},
+    { title: 'Car Insurance3', amount: 300.55, date: new Date()},
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem/>
+      {
+        expenses.map(expense =>
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        )
+      }
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const now = new Date();
-  const title = 'Car Insurance';
-  const price = 100.55;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{now.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${price}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
