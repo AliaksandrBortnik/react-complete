d7c943082282d796870b015152c45a8fa6f0b53c
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-17 16:23:55 +0300

    New expense form component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -9,7 +10,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses}/>
     </div>
   );
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,31 @@
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+  const clickHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type="text"/>
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input type='number' min='0.01' step='0 .01'/>
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input type='date' min='2022-01-01' max='2022-12-31'/>
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='submit' onClick={clickHandler}>Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
