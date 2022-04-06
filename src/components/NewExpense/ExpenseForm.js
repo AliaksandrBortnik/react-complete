4ea92f6909c75255422b5e04d035fe19dc753787
Alex Bortnik <bortnik.ar@gmail.com>
2022-04-06 21:00:28 +0400

    Simple case of usage for useRef hooks to log data

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,13 +1,25 @@
 import './ExpenseForm.css';
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 
 const ExpenseForm = (props) => {
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0.01);
   const [date, setDate] = useState('2022-02-17');
 
+  const titleInputRef = useRef();
+  const amountInputRef = useRef();
+  const dateInputRef = useRef();
+
   const onSubmitFormHandler = (e) => {
     e.preventDefault();
+
+    // TODO: basic logging using useRef hook
+    console.warn(
+      'Submitting form. Current controls values',
+      titleInputRef.current.value,
+      amountInputRef.current.value,
+      dateInputRef.current.value
+    );
 
     const formData = {
       title,
@@ -33,15 +45,15 @@ const ExpenseForm = (props) => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' value={title} onChange={onChangeTitleHandler}/>
+          <input type='text' value={title} onChange={onChangeTitleHandler} ref={titleInputRef}/>
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' value={amount} onChange={onChangeAmountHandler} min='0.01' step='0.01'/>
+          <input type='number' value={amount} onChange={onChangeAmountHandler} ref={amountInputRef} min='0.01' step='0.01'/>
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' value={date} onChange={onChangeDateHandler} min='2019-01-01' max='2022-12-31'/>
+          <input type='date' value={date} onChange={onChangeDateHandler} ref={dateInputRef} min='2019-01-01' max='2022-12-31'/>
         </div>
       </div>
       <div className='new-expense__actions'>
